ff549aba7b2398cc694c47d4b56ed5dcf307320e
Inder <crownis2010@gmail.com>
2022-06-11 22:59:03 +0530

    changes

--- app.js
+++ app.js
@@ -50,31 +50,58 @@ app.use(mongoSanitize({
 }))
 const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 
-const store = MongoDBStore.create({
-    mongoUrl:dbUrl,
+// new code starts
+// const store = MongoDBStore.create({
+//     mongoUrl:dbUrl,
+//     secret,
+//     touchAfter: 24 * 60 * 60
+//   });
+   
+//   store.on('error', function(e) {
+//     console.log(e);
+//   });
+
+//   app.use(session({
+//     store,
+//     name: "session",
+//     secret: process.env.Secret,
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//       httpOnly: true,
+//       // secure: true,
+//       expires: Date.now() + 1000 * 60 * 60 *24 * 7,
+//       maxAge: 1000 * 60 * 60 *24 * 7
+//     }
+//   })); 
+//new code ends
+
+const store = new MongoDBStore({
+    url: dbUrl,
     secret,
     touchAfter: 24 * 60 * 60
-  });
-   
-  store.on('error', function(e) {
-    console.log(e);
-  });
+});
 
-  app.use(session({
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
+
+  // old code starts
+  const sessionConfig = {
     store,
-    name: "session",
-    secret: process.env.Secret,
+    name: 'session',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
-      httpOnly: true,
-      // secure: true,
-      expires: Date.now() + 1000 * 60 * 60 *24 * 7,
-      maxAge: 1000 * 60 * 60 *24 * 7
+        httpOnly: true,
+        // secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
-  })); 
-
-//app.use(session(sessionConfig));
+}
+app.use(session(sessionConfig));
+//old code ends
 app.use(flash());
 //app.use(helmet());
 
